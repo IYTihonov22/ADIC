2a6c55dd82660576500f201ed42e5ffb874a6dcc
IYTihonov22 <IYTihonov22@codingburgas.bg>
2023-06-01 10:05:55 +0300

    Added some folders and files to work with

---
+++ scripts/index.js
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ADIC</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="header">
+        <div class="nav">
+
+            <a href="#">Home Page</a>
+            <a href="#">Contribute</a>
+            <a href="#">Comments</a>
+            <a href="#">Creators</a>
+
+        </div>
+    </div>
+
+    <div class="welcome">
+        <h1 class="welcome__text">Robots Are <br> The Future</h1>
+        <img src="images/welcome-bg.jpg" class="welcome__bg">
+    </div>
+    
+    <div class="content1__container">
+        <div class="content1">
+            <div class="content1__text">
+                <h1 >What Is A Robot?</h1>
+                <h2>A robot is a programmable machine designed to carry out specific tasks autonomously or semi-autonomously. It typically consists of mechanical components, sensors, actuators, and a control system, enabling it to interact with its environment.</h2>
+            </div>
+
+            <div class="content1__image">
+                <img src="images/desc1.jpg" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="content2__container">
+
+        <div class="content2">
+
+            <div class="content2__image">
+                <img src="images/desc2.jpg" alt="">
+            </div>
+
+            <div class="content2__text">
+                <h1 >Importance of Robotics</h1>
+                <h2>Robotics is a field that holds immense importance in today's rapidly advancing world. It combines science, engineering, and technology to create intelligent machines capable of performing complex tasks with precision and efficiency.</h2>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="content3__container">
+
+        <div class="content3">
+
+            <div class="content3__text">
+                <h1 >Applications of Robotics</h1>
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec consectetur lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque volutpat elementum augue sit amet ultricies. Lorem ipsum dolor sit id.</h2>
+            </div>
+
+            <div class="content3__image">
+                <img src="images/desc3.jpg" alt="">
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="footer">
+        <div class="footer__content">
+            <h2 class="footer__text">phone number: +359 123456789</h2>
+            <h2 class="footer__text">email: samplemail@gmail.com</h2>
+
+            <div class="footer__images">
+                <img src="images/facebook.png" class="footer__img">
+                <img src="images/instagram.png" class="footer__img">
+                <img src="images/twitter.png" class="footer__img">
+            </div>
+
+        </div>
+    </div>
+
+</body>
+</html>

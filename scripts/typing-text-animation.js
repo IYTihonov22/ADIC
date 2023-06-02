8f960466eeb1a10712870db6c128c3080c52604d
IYTihonov22 <IYTihonov22@codingburgas.bg>
2023-06-03 02:37:06 +0300

    Optimization fixed

--- scripts/typing-text-animation.js
+++ scripts/typing-text-animation.js
@@ -1,11 +1,7 @@
-var words = ['Robots Are\n The Future '],
+var letters = ['robots are\nthe future'],
     part,
     i = 0,
     offset = 0,
-    len = words.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 15,
     speed = 100;
 
 var isRunning = false; // Flag to track if the script is already running
@@ -15,38 +11,10 @@ var wordflick = function () {
     isRunning = true;
 
     var intervalId = setInterval(function () {
-      if (forwards) {
-        if (offset >= words[i].length) {
-          skip_count++;
-          if (skip_count === skip_delay) {
-            forwards = false;
-            skip_count = 0;
-          }
-        }
-      } else {
-        if (offset === 0) {
-          forwards = true;
-          i++;
-          offset = 0;
-          if (i >= len) {
-            i = 0;
-          }
-        }
-      }
-      part = words[i].substr(0, offset);
-      if (skip_count === 0) {
-        if (forwards) {
-          offset++;
-        } else {
-          offset--;
-        }
-      }
-      $('.word').text(part);
 
-      if (i === len - 1 && offset === words[i].length) {
-        clearInterval(intervalId);
-        isRunning = false;
-      }
+      part = letters[i].substr(0, offset);
+        offset++;
+      $('.word').text(part);
     }, speed);
   }
 };

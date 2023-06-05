ae44c0475fb30bf056e18f6579fd7b3c8a74d800
IYTihonov22 <IYTihonov22@codingburgas.bg>
2023-06-05 23:19:42 +0300

    Added comments

--- scripts/scroll-triger-text-animation.js
+++ scripts/scroll-triger-text-animation.js
@@ -1,47 +1,48 @@
+// Set the duration of the animation
 var animationDuration = .75;
 
-gsap.registerPlugin(ScrollTrigger);
-
+// Define a scrollObject function
 var scrollObject = function () {
-gsap.registerPlugin(ScrollTrigger);
-gsap.utils.toArray(".revealUp").forEach(function (elem) {
-  ScrollTrigger.create({
-    trigger: elem,
-    markers: false,
-    onEnter: function () {
 
+  // Register the ScrollTrigger plugin (again)
+  gsap.registerPlugin(ScrollTrigger);
+  
+  // Iterate over each element with the class "revealUp"
+  gsap.utils.toArray(".revealUp").forEach(function (elem) {
+    // Create a ScrollTrigger for each element
+    ScrollTrigger.create({
+      trigger: elem, // Set the trigger element
+      markers: false, // Disable the ScrollTrigger markers
+
+      // When the element enters the viewport
+      onEnter: function () {
+        
+        // Apply animation using GSAP fromTo method
         gsap.fromTo(
-        elem,
-        { y: 100, autoAlpha: 0 },
-        {
-          duration: animationDuration,
-          y: 0,
-          autoAlpha: 1,
-          ease: "cubic",
-          overwrite: "auto"
-        } 
-      );
-    },
-    onEnterBack: function () {
-      gsap.fromTo(
-        elem,
-        { y: 0, autoAlpha: 1 },
-        {
-          duration: animationDuration ,
-          y: 0,
-          autoAlpha: 1,
-          ease: "forward",
-          overwrite: "auto"
-        }
-      );
-    },
+          elem,
+          { y: 100, autoAlpha: 0 }, // Initial state (offset and transparency)
+          {
+            duration: animationDuration, // Set the animation duration
+            y: 0, // Final offset (move to original position)
+            autoAlpha: 1, // Fully opaque
+            ease: "cubic", // Use cubic easing function
+            overwrite: "auto" // Allow automatic overwriting
+          } 
+        );
+      },
+    });
   });
-});
 
-$(window).off("scroll");
+  // Turn off the "scroll" event listener
+  $(window).off("scroll");
 };
 
+// Scroll to the top of the page before unloading
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+// Listen for the "scroll" event on the window
 $(window).on("scroll", function () {
-    scrollObject();
-    $(window).off("scroll");
+  scrollObject(); // Call the scrollObject function
 });
--- server/saveFormData.js
+++ server/saveFormData.js
@@ -1,42 +1,52 @@
-const express = require('express');
-const fs = require('fs');
+// Importing required modules
+const express = require('express'); // Express framework for handling HTTP requests and routes
+const fs = require('fs'); // File system module for reading and writing files
 
+// Create an Express application
 const app = express();
+
+// Set the port number for the server
 const port = 3000;
 
+// Middleware to parse URL-encoded and JSON request bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specified HTTP methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specified headers
     next();
 });
 
+// Handling GET request at the root URL
 app.get('/', (req, res) => {
-  res.send('Server is running');
+  res.send('Server is running'); // Send a simple response
 })
 
+// Handling POST request to save form data
 app.post('/saveFormData', (req, res) => {
-  console.log('\x1b[33mReceived a request to save form data.\x1b[0m'); // Yellow color
+  console.log('\x1b[33mReceived a request to save form data.\x1b[0m'); // Yellow color console log
 
-  const formData = req.body;
-  console.log('\x1b[33mForm data received:\x1b[0m', formData); // Yellow color
+  const formData = req.body; // Extract the form data from the request body
+  console.log('\x1b[33mForm data received:\x1b[0m', formData); // Yellow color console log
 
-  const formDataString = JSON.stringify(formData) + ',\n';
-  console.log('\x1b[33mForm data converted to string:\x1b[0m', formDataString); // Yellow color
+  const formDataString = JSON.stringify(formData) + ',\n'; // Convert form data to a string
+  console.log('\x1b[33mForm data converted to string:\x1b[0m', formDataString); // Yellow color console log
 
+  // Append the form data string to a file named 'formData.txt'
   fs.appendFile('formData.txt', formDataString, 'utf8', (err) => {
       if (err) {
-          console.log('\x1b[31mError saving the form data:\x1b[0m', err); // Red color
-          res.sendStatus(500);
+          console.log('\x1b[31mError saving the form data:\x1b[0m', err); // Red color console log
+          res.sendStatus(500); // Send a 500 status code (Internal Server Error)
       } else {
-          console.log('\x1b[32mForm data saved successfully!\x1b[0m'); // Green color
-          res.sendStatus(200);
+          console.log('\x1b[32mForm data saved successfully!\x1b[0m'); // Green color console log
+          res.sendStatus(200); // Send a 200 status code (OK)
       }
   });
 });
+
+// Start the server and listen on the specified port
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`); // Print a message indicating the server is running
 });
